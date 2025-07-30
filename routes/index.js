e7ec6914e9f1ee9146d32f1bc41536f7375f976d
AnaisBresle <anais.bresle@gmail.com>
2025-07-30 18:20:57 +0100

    updated index routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,30 @@
 const router = require("express").Router();
 
-const postRoutes = require("./post");
-const categoryRoutes = require("./category");
-const userRoutes = require("./user");
+const approvalDecisionsRoutes = require("./approval_decisions");
+const approvalRequestsRoutes = require("./approval_requests");
+
+const filesRoutes = require("./files");
+const rolesRoutes = require("./roles");
+const usersRoutes = require("./users");
+
+const workflowStageRoutes = require("./workflow_stage");
+const workflowsRoutes = require("./workflows");
 
 // create a default route for /api
 router.get("/api", (req, res) => {
   res.json({ message: "Welcome to the API" });
 });
 
-router.use("/api/categories", categoryRoutes);
-router.use("/api/posts", postRoutes);
-router.use("/api/users", userRoutes);
+router.use("/api/approval_decisions", approvalDecisionsRoutes);
+router.use("/api/approval_requests", approvalRequestsRoutes);
+
+router.use("/api/files", filesRoutes);
+router.use("/api/roles", rolesRoutes);
+router.use("/api/users", usersRoutes);
+
+router.use("/api/workflow_stage", workflowStageRoutes);
+router.use("/api/workflows", workflowsRoutes);
+
+
 
 module.exports = router;
