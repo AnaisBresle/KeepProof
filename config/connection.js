0ed89b3b2939995900d90873b9e55b195d6ee923
AnaisBresle <anais.bresle@gmail.com>
2025-08-04 12:23:58 +0100

    adding port to fix issue

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,7 @@ const sequelize = process.env.JAWSDB_URL
       {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-         port: Number(process.env.DB_PORT) || 3306,
+         port: Number(process.env.DB_PORT) || 3306, 
       }
     );
 
